9ec9c25c75b126c15bbbe1289837829cf2c976ff
dbu99r@protonmail.com <dbu99r@protonmail.com>
2025-05-21 21:56:46 +0200

    Added view button

--- main.py
+++ main.py
@@ -2,12 +2,20 @@ import kivy
 kivy.require('1.10.0')
 
 from kivy.app import App
-#from kivy.uix.button import Label
 from kivy.uix.button import Button
+from kivy.uix.boxlayout import BoxLayout
 
 class HelloKivy(App):
-	def build(self):
-		return Button(text = "Add daily task", size=(150, 50), size_hint=(None, None), pos_hint={'center_x':0.5, 'center_y':0.5})
+    def build(self):
+        layout = BoxLayout(orientation='vertical', padding=10, spacing=10)
+
+        btn1 = Button(text="Add daily task", size_hint=(None, None), size=(200, 50))
+        btn2 = Button(text="View tasks", size_hint=(None, None), size=(200, 50))
+
+        layout.add_widget(btn1)
+        layout.add_widget(btn2)
+
+        return layout
 
 helloKivy = HelloKivy()
 helloKivy.run()
