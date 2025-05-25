dbcf5312b58388b2724e60d3a9cbff34384f6d1b
dbu99r@protonmail.com <dbu99r@protonmail.com>
2025-05-25 08:22:04 +0200

    Example login widget from the Kivy tutorial

--- main.py
+++ main.py
@@ -1,21 +1,24 @@
-import kivy
-kivy.require('1.10.0')
-
 from kivy.app import App
-from kivy.uix.button import Button
-from kivy.uix.anchorlayout import AnchorLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.label import Label
+from kivy.uix.textinput import TextInput
 
-class HelloKivy(App):
+class LoginScreen(GridLayout):
+
+    def __init__(self, **kwargs):
+        super(LoginScreen, self).__init__(**kwargs)
+        self.cols = 2
+        self.add_widget(Label(text="User name"))
+        self.username = TextInput(multiline=False)
+        self.add_widget(self.username)
+        self.add_widget(Label(text='password'))
+        self.password = TextInput(password=True, multiline=False)
+        self.add_widget(self.password)
+
+class MyApp(App):
+
     def build(self):
-        layout = AnchorLayout(anchor_x='center', anchor_y='center')
+        return LoginScreen()
 
-        btn1 = Button(text="Add daily task", size_hint=(None, None), size=(200, 50))
-        btn2 = Button(text="View tasks", size_hint=(None, None), size=(200, 50))
-
-        layout.add_widget(btn1)
-        layout.add_widget(btn2)
-
-        return layout
-
-helloKivy = HelloKivy()
-helloKivy.run()
+if __name__ == '__main__':
+    MyApp().run()
